7fcf4a18a67de408f93713f2bca49ba77fe51e2b
yezidrhome <wang_zipeng@outlook.com>
2017-06-11 10:10:17 +0800

    【更新】完成StockServiceImpl的实现

--- stock_backService/src/main/java/top/softzztiedu/service/impl/StockServiceImpl.java
+++ stock_backService/src/main/java/top/softzztiedu/service/impl/StockServiceImpl.java
@@ -1,20 +1,27 @@
 package top.softzztiedu.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.topone.entry.Stock;
+import com.topone.model.StockModel;
 
 import top.softzztiedu.result.ResultDO;
 import top.softzztiedu.service.StockService;
 @Service
 public class StockServiceImpl implements StockService {
 
+	@Autowired
+	private StockModel stockModel;
+	
 	public ResultDO getCommodityStock(Integer id) {
 		// TODO Auto-generated method stub
 		ResultDO resultDO = new ResultDO();
 		// 获取商品库存
-
+		Stock stock =stockModel.getById(id);
 		resultDO.setSuccess(true);
 		resultDO.setMessage("获取商品库存成功");
-		resultDO.setResult(3453);
+		resultDO.setResult(stock);
 		return resultDO;
 	}
 
