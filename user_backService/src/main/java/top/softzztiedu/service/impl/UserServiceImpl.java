9899839113daf7d065367e35ce16a7105d7fcd71
yezidrhome <wang_zipeng@outlook.com>
2017-06-11 10:03:30 +0800

    【更新】完成 UserServiceImpl的实现

--- user_backService/src/main/java/top/softzztiedu/service/impl/UserServiceImpl.java
+++ user_backService/src/main/java/top/softzztiedu/service/impl/UserServiceImpl.java
@@ -1,27 +1,33 @@
 package top.softzztiedu.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.topone.entry.Member;
+import com.topone.model.MemberModel;
+
 import top.softzztiedu.exception.ServiceException;
-import top.softzztiedu.model.Member;
 import top.softzztiedu.result.ResultDO;
 import top.softzztiedu.service.UserService;
 @Service
 public class UserServiceImpl implements UserService{
 
+	@Autowired
+	private MemberModel memberModel;
+	
 	public ResultDO login(String name, String pwd) throws ServiceException {
 		// TODO Auto-generated method stub
-		
 		ResultDO resultDO = new ResultDO();
-		if(name.equals(pwd)){
+		int login = memberModel.login(name, pwd);
+		if(login!=0){
 			resultDO.setSuccess(true);
 			resultDO.setMessage("登陆成功");//登陆成功
-			resultDO.setResult(new Member());//登陆成功带有用户对象
+			Member member = memberModel.getById(name);
+			resultDO.setResult(member);//登陆成功带有用户对象
 		}else{
 			resultDO.setSuccess(false);
 			resultDO.setMessage("登陆失败");//登陆失败
 		}
-		
 		return resultDO;
 	}
 
@@ -29,7 +35,8 @@ public class UserServiceImpl implements UserService{
 		// TODO Auto-generated method stub
 		Member member = (Member) object;
 		ResultDO resultDO = new ResultDO();
-		if(member.getName().equals("王子鹏")){
+		int add = memberModel.add(member);
+		if(add==1){
 			resultDO.setSuccess(true);
 			resultDO.setMessage("注册成功");//成功
 		}else{
