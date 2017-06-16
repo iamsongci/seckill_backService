03790d2d9beb1ddabc443088aa9fb8ca9f561139
yezidrhome <wang_zipeng@outlook.com>
2017-06-16 08:47:29 +0800

    【更新】 完善交易服务实现

--- business_backService/src/main/java/top/softzztiedu/service/impl/BusinessServiceImpl.java
+++ business_backService/src/main/java/top/softzztiedu/service/impl/BusinessServiceImpl.java
@@ -25,102 +25,101 @@ public class BusinessServiceImpl implements BusinessService {
 
 	public ResultDO addShoppingCart(Shopping shopppig) throws ServiceException {
 		// TODO Auto-generated method stub
+		boolean success = false;
 		ResultDO resultDO = new ResultDO();
-		int add = shoppingModel.add(shopppig);
-		if (add ==1) {
-			resultDO.setSuccess(true);
-			resultDO.setMessage("加入购物车成功");
-		} else {
-			resultDO.setSuccess(false);
-			resultDO.setMessage("加入购物车失败");
+		if(shopppig!=null){
+			int add = shoppingModel.add(shopppig);
+			if (add ==1) {
+				resultDO.setMessage("加入购物车成功");
+			} else {
+				resultDO.setMessage("加入购物车失败");
+			}
+		}else{
+			resultDO.setMessage("shopppig为空值");
 		}
+		resultDO.setSuccess(success);
 		return resultDO;
 	}
 
 	public ResultDO toPay(Integer id, Integer orderStats) throws ServiceException {
 		// TODO Auto-generated method stub
+		boolean success = false;
 		ResultDO resultDO = new ResultDO();
 		// 改变订单状态
-		int update = orderModel.update(id, orderStats);
-		if (update==1) {
-			resultDO.setSuccess(true);
-			resultDO.setMessage("改变订单状态成功");
-		} else {
-			resultDO.setSuccess(false);
-			resultDO.setMessage("改变订单状态失败");
+		if(id!=null&&orderStats!=null){
+			int update = orderModel.update(id, orderStats);
+			if (update==1) {
+				resultDO.setMessage("改变订单状态成功");
+			} else {
+				resultDO.setMessage("改变订单状态失败");
+			}
+		}else{
+			resultDO.setMessage("id空值");
 		}
+		resultDO.setSuccess(success);
 		return resultDO;
 	}
 
 	public ResultDO getMyStoreOrder(Integer sellerId) {
 		// TODO Auto-generated method stub
+		boolean success = false;
 		ResultDO resultDO = new ResultDO();
-		// 获取店铺订单
-		List<Order> orders = new ArrayList<Order>();
-		
-		for(int i=0;i<=10;i++){
-			Order order = new Order();
-			order.setId(i);
-			order.setQuantum(i);
-			order.setBuyersId(i);
-			order.setSellerId(i);
-			BigDecimal b = new BigDecimal(100.2+i);
-			order.setSum(b);
-			order.setAddess("郑州市中原区"+i);
-			orders.add(order);
+		if(sellerId!=null){
+			// 获取店铺订单
+			List<Order> orders = new ArrayList<Order>();
+			int count = orderModel.countByShellerId(sellerId);
+			orders = orderModel.getAllBySheller(0, count, sellerId);
+			if (orders!=null) {
+				resultDO.setMessage("获取店铺订单成功");
+				resultDO.setResult(orders);
+			} else {
+				resultDO.setMessage("获取店铺订单失败");
+			}
+		}else{
+			resultDO.setMessage("sellerId为空！！");
 		}
-		
-		if (true) {
-			resultDO.setSuccess(true);
-			resultDO.setMessage("获取店铺订单成功");
-			resultDO.setResult(orders);
-		} else {
-			resultDO.setSuccess(false);
-			resultDO.setMessage("获取店铺订单失败");
-		}
+		resultDO.setSuccess(success);
 		return resultDO;
 	}
 
 	public ResultDO addOrder(Order order) throws ServiceException {
 		// TODO Auto-generated method stub
+		boolean success = false;
 		ResultDO resultDO = new ResultDO();
 		// 生成订单
-		int add = orderModel.add(order);
-		if (add==1) {
-			resultDO.setSuccess(true);
-			resultDO.setMessage("生成订单成功");
-		} else {
-			resultDO.setSuccess(false);
-			resultDO.setMessage("生成订单失败");
+		if(order!=null){
+			int add = orderModel.add(order);
+			if (add==1) {
+				resultDO.setMessage("生成订单成功");
+			} else {
+				resultDO.setMessage("生成订单失败");
+			}
+		}else{
+			resultDO.setMessage("传入空值！！");
 		}
+		resultDO.setSuccess(success);
 		return resultDO;
 	}
 
 	public ResultDO getMyOrderList(Integer buyersId) {
 		// TODO Auto-generated method stub
+		boolean success = false;
 		ResultDO resultDO = new ResultDO();
-		// 获取我的订单
-		List<Order> orders = new ArrayList<Order>();
-		for(int i=0;i<=10;i++){
-			Order order = new Order();
-			order.setId(i);
-			order.setQuantum(i);
-			order.setBuyersId(i);
-			order.setSellerId(i);
-			BigDecimal b = new BigDecimal(100.2+i);
-			order.setSum(b);
-			order.setAddess("郑州市中原区"+i);
-			orders.add(order);
+		if(buyersId!=null){
+			// 获取店铺订单
+			List<Order> orders = new ArrayList<Order>();
+			int count = orderModel.countByBuyId(buyersId);
+			orders = orderModel.getAll(0, count, buyersId);
+			if (orders!=null) {
+				resultDO.setMessage("获取我的订单成功");
+				resultDO.setResult(orders);
+			} else {
+				resultDO.setMessage("获取我的订单失败");
+			}
+		}else{
+			resultDO.setMessage("buyersId为空！！");
 		}
-
-		if (true) {
-			resultDO.setSuccess(true);
-			resultDO.setMessage("获取我的订单成功");
-			resultDO.setResult(orders);
-		} else {
-			resultDO.setSuccess(false);
-			resultDO.setMessage("获取我的订单失败");
-		}
+		resultDO.setSuccess(success);
 		return resultDO;
 	}
 
